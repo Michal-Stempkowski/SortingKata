dabb09ea7da8ffcdef06ec21515c06d12237ad29
MichalStempkowski <michulix@gmail.com>
2014-12-07 20:43:11 +0100

    self.assertEquals([1, 2], sort([2, 1])) - pivot approach

--- sorting/test_sorting_kata.py
+++ sorting/test_sorting_kata.py
@@ -1,8 +1,23 @@
+from itertools import chain
+from random import randrange
 from unittest import TestCase
 
 
-def sort(list):
-    return list
+def select_random_pivot(unsorted):
+    return unsorted[randrange(0, len(unsorted))]
+
+
+def sort(unsorted):
+    if unsorted and len(unsorted) > 1:
+        pivot = select_random_pivot(unsorted)
+
+        lesser = filter(lambda x: x < pivot, unsorted)
+        equal = filter(lambda x: x == pivot, unsorted)
+        greater = filter(lambda x: x > pivot, unsorted)
+
+        return list(chain(lesser, equal, greater))
+
+    return unsorted
 
 
 class TestSortingKata(TestCase):
@@ -14,3 +29,4 @@ class TestSortingKata(TestCase):
 
     def test_given_empty_list__should_return_empty_list(self):
         self.assertEquals([], sort([]))
+        self.assertEquals([1, 2], sort([2, 1]))
