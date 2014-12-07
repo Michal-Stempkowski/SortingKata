5ebfd26428bce5e2659d4f7b41bdb84c222fa078
MichalStempkowski <michulix@gmail.com>
2014-12-07 19:21:57 +0100

    given_none__should_return_none

--- sorting/test_sorting_kata.py
+++ sorting/test_sorting_kata.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
 
 
+def sort(list):
+    return None
+
+
 class TestSortingKata(TestCase):
     def setUp(self):
         pass
 
-    def test_nothing(self):
-        self.assertTrue(True)
+    def test_given_none__should_return_none(self):
+        self.assertEquals(None, sort(None))
